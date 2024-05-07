a7871a065abf8078103375140a8c13f4b4a16838
matheus0908 <149191886+matheus0908@users.noreply.github.com>
2024-05-07 16:52:53 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+
+$dbHost = 'Localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'produção';
+
+$conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+//if($conexao->connect_errno)
+//{
+//   echo "Erro";
+//}
+
+//else
+//{
+//    echo "Conexão efetuada com sucesso";
+//}
+
+?>
